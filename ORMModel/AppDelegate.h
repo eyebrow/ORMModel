d30b2689f0888364b75d8be75173ee0620351628
剑眉枉凝 <jianmeiwangning@gmail.com>
2016-07-15 10:55:01 +0800

    Initial Commit

---
+++ ORMModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ORMModel
+//
+//  Created by iprincewang on 16/7/15.
+//  Copyright © 2016年 com.tencent.prince. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
